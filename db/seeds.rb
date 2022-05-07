4f813c66d3b45fbc9885211a0cf82e5e01b30cbd
Thonney Cedric <c.thonney@gmail.com>
2022-05-07 19:20:23 +0400

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Pet.destroy_all
+
+Pet.create(name: "Fido", species: "dog", address: "123 Main St", found_on: Date.today - 1)
+Pet.create(name: "Buddy", species: "cat", address: "123 Main St", found_on: Date.today - 2)
+Pet.create(name: "Barker", species: "rabbit", address: "123 Main St", found_on: Date.today - 3)
+Pet.create(name: "Fizz", species: "snake", address: "123 Main St", found_on: Date.today - 4)
+Pet.create(name: "Turtle", species: "turtle", address: "123 Main St", found_on: Date.today - 5)
+puts "Created #{Pet.count} pets."
